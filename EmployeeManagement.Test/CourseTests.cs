4c00460ad75c23b181ee4e688f438b91ed91f211
Yoni Hirshowitz <jonhirsh39@gmail.com>
2023-06-07 21:08:44 -0400

    added couple of tests

---
+++ EmployeeManagement.Test/CourseTests.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class CourseTests
+    {
+        [Fact]
+        public void CourseConstructor_IsNewMustBeTrue() {
+            //Act
+            var course = new Course("Distater Management");
+
+            //Assert 
+            Assert.True(course.IsNew);
+        }
+    }
+}
---
+++ EmployeeManagement.Test/EmployeeTests.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstNameAndLastName_FullNameIsConcatenation()
+        {
+            //Arrange
+            var employee = new InternalEmployee("yoni", "witz", 0, 2500, false, 1);
+
+            //Act
+            //employee.FirstName = "Lucia";  
+            //employee.LastName = "Shelton";
+
+            //Assert
+            Assert.Equal("yoni witz", employee.FullName);
+        }
+    }
+}
